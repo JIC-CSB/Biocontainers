7224a92f33530ccf89f56b81acb33baa5dbd840f
Matthew Hartley <mhartley@cantab.net>
2017-02-06 11:30:14 +0000

    Fix tags in image FROM statements

--- build_all.sh
+++ build_all.sh
@@ -4,4 +4,5 @@ for DIR in `ls -d */`; do
     NAME=`echo $DIR | cut -d'_' -f2`
     TRIMMED=${NAME%/}
     echo docker build -t jicscicomp/$TRIMMED $DIR
+    docker build -t jicscicomp/$TRIMMED $DIR
 done
